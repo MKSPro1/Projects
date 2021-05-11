5d1781c2fbae16a8814455d2bc1325992c4b7915
MKSPro1 <81444444+MKSPro1@users.noreply.github.com>
2021-05-11 16:26:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿
+// This is the side that gets user input
+
+using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void methodOrder(Vend order)
+        {
+            order.OrderItem("");
+        }
+        static void methodNewBalance(Vend balance)
+        {
+            balance.KeepBalance(0.00);
+        }
+        static void methodRefund(Vend change)
+        {
+            change.GiveRefund();
+        }
+        static void methodCoinDenom(Vend vend)
+        {
+            Console.WriteLine("Please insert a coin");
+            string coinDenom = Console.ReadLine();
+            bool isDoubleCoin = double.TryParse(coinDenom, out double coinDenomResult);
+
+            if (isDoubleCoin)
+            {
+                vend.CoinInsert(coinDenomResult);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid coin");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Vend select1 = new Vend();
+            while (true)
+            {
+                Console.WriteLine("Type \n 'I' to Insert Coin \n 'R' to Refund \n 'O' to Order Item \n 'E' to Exit");
+                string option = Console.ReadLine();
+                if (option.ToUpper() == "I")
+                {
+                    methodCoinDenom(select1);
+                }
+                else if (option.ToUpper() == "R")
+                {
+                    methodRefund(select1);
+                    break;
+                }
+                else if (option.ToUpper() == "O")
+                {
+                    methodOrder(select1);
+                    {
+                        methodNewBalance(select1);
+                    }
+                }
+                else if (option.ToUpper() == "E")
+                {
+                    methodRefund(select1);
+                    break;
+
+                    // Console.WriteLine("Thank you for your business!");
+                }
+                else
+                {
+                    Console.WriteLine("Please input a valid option");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Vending.cs
@@ -0,0 +1,79 @@
+﻿
+
+
+using System;
+namespace VendingMachine
+{
+    public class Vend
+    {
+        double beginningBalance = 0;
+        double price;
+        double newBalance;
+
+        public Vend()
+        {
+
+        }
+        public void CoinInsert(double tendered) // Summing the coins inserted
+        {
+            if (tendered == .05 || tendered == .10 || tendered == .25 || tendered == .50 || tendered == 1)
+            {
+                this.beginningBalance += tendered;
+                Console.WriteLine("You inserted: $" + String.Format("{0:0.00}", tendered) + "\nYour available balance is: $" + String.Format("{0:0.00}", beginningBalance) + " Please choose another option");
+            }
+            else
+            {
+                Console.WriteLine("Please insert a valid coin");
+            }
+        }
+        public void OrderItem(string item)
+        {
+            Console.WriteLine(" 1 Chips \n 2 Nuts \n 3 Candy \n 4 Beef Jerkey \n 5 Refund");
+            string itemSelect = Console.ReadLine();
+            bool isValidItem = int.TryParse(itemSelect, out int itemSelectResult);
+
+            if (itemSelectResult == 1)
+            {
+                price = .50;
+                Console.WriteLine("You selected " + itemSelectResult + " - Chips - The price is $" + String.Format("{0:0.00}", price));
+            }
+            else if (itemSelectResult == 2)
+            {
+                price = .75;
+                Console.WriteLine("You selected " + itemSelectResult + " - Nuts - The price is $" + String.Format("{0:0.00}", price));
+            }
+            else if (itemSelectResult == 3)
+            {
+                price = 1.00;
+                Console.WriteLine("You selected " + itemSelectResult + " - Candy - The price is $" + String.Format("{0:0.00}", price));
+            }
+            else if (itemSelectResult == 4)
+            {
+                price = 1.25;
+                Console.WriteLine("You selected " + itemSelectResult + " - Beef Jerkey - The price is $" + String.Format("{0:0.00}", price));
+            }
+            else if (itemSelectResult == 5)
+            {
+                price = 0.00;
+                //Console.WriteLine("Your refund is $" + String.Format("{0:0.00}", newBalance));
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid item number");
+            }
+        }
+        public void GiveRefund()
+        {
+            Console.WriteLine("Your refund is $" + String.Format("{0:0.00}", newBalance + "\nThank you!"));
+        }
+        public double KeepBalance(double balance)
+        {
+            newBalance = beginningBalance -= price;
+
+            Console.WriteLine("Your new balance is $" + String.Format("{0:0.00}", newBalance));
+            {
+                return newBalance;
+            }
+        }
+    }
+}
